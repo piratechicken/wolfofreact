d415533bfda8a54d03adfb804ceaab94c8c6a6ae
Simbidion <simonadwyer@yahoo.com.au>
2017-11-30 11:35:55 +1100

    added iex api call

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
 import React, { Component } from 'react';
 import StockInfo from './components/Stockinfo'
+import { loadQuoteForStock } from './api/iex'
 import './App.css';
 
+
 class App extends Component {
+  state = {
+    error: null,
+    quote: null
+  }
+
+  // The first time our component is rendered, this method is called
+  componentDidMount() {
+    loadQuoteForStock('nfx')
+      .then((data) => {
+        this.setState({ quote: data })
+      })
+      .catch((error) => {
+        if (error.response.status === 404) {
+          error = new Error('The stock symbol does not exist')
+        }
+        console.error(error.message)
+        this.setState({ error: error })
+      })
+  }
+
   render() {
+    // const quote = this.state.quote
+    const { error, quote } = this.state
+
     return (
       <div className="App">
         <h1>Wolf of React</h1>
-        <StockInfo 
-            symbol = { 'nfs' }
-            companyName = { '4' }
-            primaryExchange = { '%4' }
-            latestPrice = { '$5' }
-            latestSource = { 'somewhere' }
-            week52High = { '5.5' }
-            week52Low = { '4.4' }
-        />
+        {
+          !!error &&
+            <div className="error">{ error.message }</div>
+        }
+        {
+          !!quote ? (
+            <StockInfo 
+              { ...quote }
+            />
+          ) : (
+            <div className="loading">... loading ...</div>
+          )
+        }
       </div>
     );
   }
